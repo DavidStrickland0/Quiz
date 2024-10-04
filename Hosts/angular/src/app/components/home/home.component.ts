3399a22aefce0c34831d5031a09adad783da824d
David Strickland <David.Strickland@wellfit.com>
2024-10-04 00:42:33 -0500

    Add menu

--- Hosts/angular/src/app/components/home/home.component.ts
+++ Hosts/angular/src/app/components/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
 import { CommonModule } from '@angular/common';
 import { Title } from '@angular/platform-browser';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   standalone: true,
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  imports: [MatExpansionModule, CommonModule], // Ensure expansion modules are imported
+  imports: [MatExpansionModule, CommonModule, MenuComponent], // Ensure expansion modules are imported
 })
 export class HomeComponent {
   constructor(  private titleService: Title ){}
---
+++ Hosts/angular/src/app/components/menu/menu.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
